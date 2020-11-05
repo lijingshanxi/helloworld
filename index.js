35cf91b1be71d592db9ab24381e3b579a5d5e64a
jing.li <jing.li@advantest.com>
2020-11-05 15:00:01 +0800

    hello world

---
+++ index.js
@@ -0,0 +1,31 @@
+const AWS = require('aws-sdk');
+
+/**
+ * Demonstrates a simple HTTP endpoint using API Gateway. You have full
+ * access to the request and response payload, including headers and
+ * status code.
+ *
+ * To scan a DynamoDB table, make a GET request with the TableName as a
+ * query string parameter. To put, update, or delete an item, make a POST,
+ * PUT, or DELETE request respectively, passing in the payload to the
+ * DynamoDB API as a JSON body.
+ */
+exports.handler = async (event, context) => {
+    console.log('lijing_test: v0.1 Received event:', JSON.stringify(event, null, 2));
+
+    let body;
+    let statusCode = '200';
+    const headers = {
+        'Content-Type': 'application/json',
+    };
+
+    body = '{ "key1" : "value1" }';
+    return {
+        statusCode,
+        body,
+        headers,
+    };
+};
+
+
+
